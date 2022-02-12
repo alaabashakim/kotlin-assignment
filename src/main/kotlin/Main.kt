a652a2b065cf3476cf205d9366732f568e8178d3
alaabashakim <alaabashakim@gmail.com>
2022-02-12 10:08:05 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main (){
+    var name = "Alla Abass"
+    println(name)
+    var age = "23"
+    println(age)
+    var phone = "0746382214"
+    println(phone)
+    var weight = "50"
+    println(weight)
+
+    val iskenyan: Boolean = true
+    val isnotkenyan: Boolean =false
+    println(iskenyan)  //outputs true
+    println(isnotkenyan)  //outputs false
+}
+
+
